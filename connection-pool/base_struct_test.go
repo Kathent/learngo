3a17f1ba3354cd884c69c6c3306a829e4b816598
liangkj <liangkaijun@icsoc.net>
2017-09-08 16:38:38 +0800

    1. test etcd

--- connection-pool/base_struct_test.go
+++ connection-pool/base_struct_test.go
@@ -3,6 +3,7 @@ package connection_pool
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"sync"
 )
 
 func Test_Poll_Get(t *testing.T){
@@ -50,4 +51,36 @@ func TestNewList_Func(t *testing.T) {
 
 	assert.Equal(t, little, newList.take().(*node).data)
 	assert.Equal(t, big, newList.take().(*node).data)
+}
+
+func BenchmarkNewList_Test1(b *testing.B) {
+	n := 10000
+	a := make([]*sync.WaitGroup, 0, 10)
+	for i := 0; i < b.N; i++ {
+		tmp := &sync.WaitGroup{}
+		tmp.Add(n)
+		for j := 0; j < n; j++ {
+			go func() {
+				testList.add(j)
+			}()
+
+			go func() {
+				testList.take()
+				tmp.Done()
+			}()
+		}
+
+		a = append(a, tmp)
+	}
+
+	for _, v := range a {
+		v.Wait()
+	}
+}
+
+var testList *safeList
+func init(){
+	testList = NewList(10, func(n1, n2 *node) bool {
+		return n1.data.(int) < n2.data.(int)
+	})
 }
--- learn_etcd/learn_etcd.go
+++ learn_etcd/learn_etcd.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/coreos/etcd/clientv3"
+	"log"
 )
 
 var cl *clientv3.Client
@@ -62,7 +63,7 @@ func LoadClient(addr string) error{
 }
 
 func LearnEtcd(){
-	err := LoadClient("192.168.96.140:2379")
+	err := LoadClient("192.168.96.131:2379")
 	if err != nil{
 		panic(err)
 	}
@@ -85,4 +86,54 @@ func LearnEtcd(){
 	}
 
 	fmt.Println(fmt.Sprintf("get value is...%v",val))
+
+	grant, grantErr := cl.Grant(context.Background(), 4)
+	if grantErr != nil {
+		panic(grantErr)
+	}
+
+	resp, respErr := cl.Put(context.Background(), "ttlKey", "ttlVal", clientv3.WithLease(grant.ID))
+	if respErr != nil {
+		panic(respErr)
+	}
+
+	log.Println(fmt.Sprintf("put resp %v", resp))
+
+	get, getErr := cl.Get(context.Background(), "ttlKey")
+	if getErr != nil {
+		panic(getErr)
+	}
+
+	log.Println(fmt.Sprintf("get resp %v", get))
+
+	_, onceErr := cl.KeepAliveOnce(context.Background(), grant.ID)
+	if onceErr != nil {
+		panic(onceErr)
+	}
+
+
+	get, getErr = cl.Get(context.Background(), "ttlKey")
+	if getErr != nil {
+		panic(getErr)
+	}
+	log.Println(fmt.Sprintf("get resp %v", get))
+
+
+	get, getErr = cl.Get(context.Background(), "ttlKey")
+	if getErr != nil {
+		panic(getErr)
+	}
+	log.Println(fmt.Sprintf("get resp %v", get))
+
+	alive, aliveErr := cl.KeepAlive(context.Background(), grant.ID)
+	if aliveErr != nil {
+		panic(aliveErr)
+	}
+
+	for true {
+		select {
+			case r :=<- alive:
+			log.Println("res", r)
+		}
+	}
 }
