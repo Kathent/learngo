7bccb450c83f89db753ef5ccdc18b7b5ad3869d7
liangkj <liangkaijun@icsoc.net>
2017-10-10 20:17:00 +0800

    1. test etcd

--- go-micro-learn/go_micro_server.go
+++ go-micro-learn/go_micro_server.go
@@ -1,8 +1,20 @@
 package go_micro_learn
 
-import "github.com/micro/go-micro"
+import (
+	"github.com/micro/go-micro"
+	"github.com/micro/go-plugins/registry/etcd"
+	registry2 "github.com/micro/go-micro/registry"
+)
+const(
+	ETCD_ADDR = "192.168.96.140:2379"
+)
 
 func StartServer(){
-	micro.NewService(micro.Name("hello"),
-		micro.Version("latest"))
+	registry := etcd.NewRegistry(registry2.Addrs(ETCD_ADDR))
+
+	service := micro.NewService(micro.Name("hello"),
+		micro.Version("latest"),
+		micro.Registry(registry))
+
+	service.Init()
 }
