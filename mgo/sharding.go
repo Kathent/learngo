016622300e05bc2e56a070f65b2aa9d60f046614
liangkj <liangkaijun@icsoc.net>
2018-06-06 15:48:51 +0800

    1. server

--- mgo/sharding.go
+++ mgo/sharding.go
@@ -5,7 +5,62 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"strconv"
 	"fmt"
+	"time"
+	"math/rand"
 )
+
+func InsertNew(size int) {
+	session, err := mgo.Dial("192.168.96.224:27017")
+	if err != nil {
+		panic(err)
+	}
+	db := session.DB("sharding_test")
+	c := db.C("sharding_collection")
+	index := 0
+	bulk := c.Bulk()
+
+	vccId := []int{782, 10010, 332, 445}
+	for i := 0; i < size; i++ {
+		unix := time.Now().Unix()
+		bulk.Insert(bson.M{"vcc_id": vccId[rand.Intn(len(vccId))], "start_time": unix, "rand_val": rand.Int63n(unix)})
+		index++
+		if index >= 10000 {
+			index = 0
+			result, err := bulk.Run()
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(result)
+			bulk = c.Bulk()
+		}else if i >= size {
+			index = 0
+			result, err := bulk.Run()
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(result)
+		}
+	}
+}
+
+func  QueryMongo(collection *mgo.Collection, size int) {
+	st := struct {
+		VccId int `bson:"vcc_id"`
+		StartTime int64 `bson:"start_time"`
+		RandVal int64 `bson:"rand_val"`
+	}{}
+
+	for i := 0; i < size; i++ {
+		randN := rand.Int63n(time.Now().Unix())
+		find := collection.Find(bson.M{"vcc_id": rand.Intn(4), "start_time": randN,
+			"rand_val": randN}).One(&st)
+		if find != mgo.ErrNotFound {
+			panic(find)
+		}
+	}
+}
 
 func Insert() {
 	session, err := mgo.Dial("192.168.96.204:30031")
--- mgo/sharding_test.go
+++ mgo/sharding_test.go
@@ -1,6 +1,10 @@
 package mgo
 
-import "testing"
+import (
+	"testing"
+	"time"
+	"gopkg.in/mgo.v2"
+)
 
 func TestInsert(t *testing.T) {
 	Insert()
@@ -8,4 +12,27 @@ func TestInsert(t *testing.T) {
 
 func TestDeleteAll(t *testing.T) {
 	DeleteAll()
+}
+
+func TestInsertNew(t *testing.T) {
+	start := time.Now().Unix()
+	InsertNew(10000000)
+	end := time.Now().Unix()
+
+	t.Logf("consume time:%d", end - start)
+}
+
+func BenchmarkQueryMongo(b *testing.B) {
+	session, err := mgo.Dial("192.168.96.224:27017")
+	if err != nil {
+		panic(err)
+	}
+	db := session.DB("sharding_test")
+	c := db.C("sharding_collection")
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			QueryMongo(c, 1)
+		}
+	})
 }
