cf9dbe3edd2b4c74d3cadfbb1aacc56956afcd29
liangkj <liangkaijun52013@gmail.com>
2018-06-19 21:33:36 +0800

    mq rpc

--- enterance.go
+++ enterance.go
@@ -1,6 +1,6 @@
 package main
 
-import "learngo/gc-experiment"
+//import "learngo/learn_etcd"
 
 func main() {
 	//q := queue.New()
@@ -17,7 +17,6 @@ func main() {
 	//go_micro_learn.StartGinServer()
 	//go_micro_learn.StartServer()
 	//go go_micro_learn.StartGrpcServer()
-	//go_micro_learn.StartHelloServer()
 	//time.Sleep(time.Second * 4)
 	//go_micro_learn.StartGrcpClient()
 	//file_read_analysis.LoadFiles("load_file_dir", "2017-10-02 10:00:00", "2017-10-03 14:00:00")
@@ -25,7 +24,27 @@ func main() {
 	//mgo.TryMongoDial()
 	//yml.LeanYml()
 	//mgo.LearnDate()
-	gc_experiment.GcLoop()
-	//sharding_server.InitDb()
-	//sharding_server.Init()
+
+	arr := make([]int, 3, 5)
+	println(&arr)
+	modify(arr)
+
+	//print(arr[2])
+	println(&arr)
+}
+
+func modify(ints []int) {
+	ints[0] = 1
+	ints[1] = 2
+	ints[2] = 3
+	println(&ints)
+
+	ints = append(ints, 6)
+	println(&ints)
+
+	ints = append(ints, 6)
+	ints = append(ints, 6)
+	ints = append(ints, 6)
+	ints = append(ints, 6)
+	println(&ints)
 }
